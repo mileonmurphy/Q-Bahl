709980f42837647ab6094cd11734650baf3effe0
Miguel Leon Murphy <mileonmurphy@gmail.com>
2016-04-30 00:45:46 -0400

    Assets, Level Created

---
+++ Assets/Scripts/TeleportPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportPlayer : MonoBehaviour {
+
+	public GameObject player, teleportPoint;
+	public Camera myCamera;
+	public Vector3 teleportPos, playerPos, cameraPos;
+
+
+	// Use this for initialization
+	void Start () {
+		teleportPos = teleportPoint.transform.position;
+		playerPos = player.transform.position;
+		cameraPos = myCamera.transform.position;
+	}
+	public void OnTriggerEnter(Collider other)
+	{
+		playerPos = teleportPos;
+		cameraPos = teleportPos;
+	}
+}
