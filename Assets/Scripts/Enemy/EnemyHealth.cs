d4ab79391962f03208f881fbb105ec503384f8a6
Dan <dmh-26@charter.net>
2016-05-01 15:05:49 -0700

    Added enemy prefabs and breakable wall

---
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour {
+
+	public int maxHealth;
+	public GameObject damageParticles;
+	public int gooDrop;
+	public GameObject drop;
+
+	public AudioClip hitSound;
+	public AudioClip deathSound;
+
+	public int currentHealth;
+
+	public Slider enemyHealthIndicator;
+	public AudioSource enemyAS;
+
+	// Use this for initialization
+	void Start () {
+		currentHealth = maxHealth;
+		enemyHealthIndicator = GetComponentInChildren<Slider> ();
+		enemyHealthIndicator.gameObject.SetActive (false);
+		enemyHealthIndicator.maxValue = maxHealth;
+		enemyHealthIndicator.value = currentHealth;
+		//enemyAS = GetComponent<AudioSource> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.GetComponentInChildren<Canvas>().transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 0));
+		enemyHealthIndicator.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 0));
+	}
+
+	public void addDamage(int damage){
+		//AudioSource.PlayClipAtPoint (hitSound, transform.position);
+
+		enemyHealthIndicator.gameObject.SetActive (true);
+		if (damage <= -1) {
+			Debug.Log ("Adding health to enemy Number should be positive");
+		}
+		currentHealth -= damage;
+		enemyHealthIndicator.value = currentHealth;
+		if (currentHealth <= 0) {
+			MakeDead ();
+		}
+	}
+
+	void MakeDead(){
+		//AudioSource.PlayClipAtPoint (deathSound, transform.position);
+
+		Destroy (this.gameObject);
+		for (int i = 0; i < gooDrop; i++) {
+			Instantiate (drop, transform.position, transform.rotation);
+		}
+	}
+}
---
+++ Assets/Scripts/Player/Environment/ShardDestroy.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShardDestroy : MonoBehaviour {
+
+	bool destroyAllShards;
+
+	GameObject[] shards;
+
+	// Use this for initialization
+	void Start () {
+		destroyAllShards = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (destroyAllShards) {
+			shards = GameObject.FindGameObjectsWithTag ("Shard");
+			Invoke ("DestroyShards", 3);
+		}
+
+	}
+
+	public void MakeDestroy(){
+		destroyAllShards = true;
+	}
+
+	void DestroyShards(){
+		for (int i = 0; i < shards.Length; i++) {
+			Destroy (shards [i]);
+		}
+		destroyAllShards = false;
+	}
+}
